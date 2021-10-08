02fd53b726342b9e3a5c3fd01ea8129abd5c3563
dlhjel <dlhjel@gmail.com>
2021-10-08 10:19:01 -0400

    still a work in progress

--- modules/plotdfall.r
+++ modules/plotdfall.r
@@ -1,11 +1,50 @@
+set.seed(1)
+DF <- data.frame(2000:2009,matrix(rnorm(50, 1000, 200), ncol=5))
+colnames(DF) <- c('Year', paste0('Country', 2:ncol(DF)))
+DF.TS <- ts(DF[-1], start = 2000, frequency = 1)
+DF.TS
+# All countries in one plot... colorful, common scale, and so on
+plot(DF.TS, plot.type="single", col = 1:ncol(DF.TS))
+legend("bottomleft", colnames(DF.TS), col=1:ncol(DF), lty=1, cex=.65)
+
+# All countries in one plot... colorful, common scale, and so on
+plot(twr, plot.type="single", col = 1:ncol(twr))
+legend("bottomleft", legend=colnames(twr), col=1:ncol(twr), lty=1, cex=.65)
+data.frame(symbols=colnames(twr), color=palette()[1:ncol(twr)])
+mtext(text='test', side=3)
+
+plot(twr[,which(grepl('SPY', colnames(twr)))], plot.type="single", col = 1:ncol(twr))
+
+
+# All countries in one plot... colorful, common scale, and so on
+twrts <- as.ts(twr)
+plot(twrts, plot.type="single", col = 1:ncol(twrts))
+legend("topleft", legend=colnames(twrts), col=1:ncol(twrts), lty=1, cex=.65)
+grid(col='grey70')
+
+
+plotxts <- function(xts) {
+    date <- zoo::index(xts)
+    matplot(x=date, type='l', xaxt='n')
+
+timelabels<-format(timestamp,"%H:%M")
+axis(1,at=timestamp,labels=timelabels)
+    
+    grid(col='grey70')
+    legend('topleft',
+           legend = names(xts),
+           col    = 1:ncol(xts),
+           lty    = 1)
+}
+
 plotdfall <- function(df, xx, size=0.01, type='b', legendloc='topleft') {
     ## plots every parameter in df as a function of x on single plot
     ## df      = dataframe
     ## xx      = x-axis variable to be specified in quotes
     ## size    = datapoint size (0.01 default to not see points but still get line color)
     ## type    = 'p' for points
-    ##         = 'l' for lines
-    ##         = 'b' for both
+    ##         = 'l' for lines     <-- creates a single line so only black
+    ##         = 'b' for both      <-- same as above
     
     ## determine location of xx in the dataframe
     xxcol <- which(grepl(xx, names(df)))  ## xx column
@@ -49,3 +88,9 @@ plotdfall <- function(df, xx, size=0.01, type='b', legendloc='topleft') {
 #plotdfall(mtcars, 'mpg', legendloc='topright', type='b')
 #df <- select(mtcars, mpg, hp)
 #plotdfall(df, 'mpg', legendloc='topright', type='b')
+
+out <- equityget(c('SPY', 'IWM', 'EFA', 'AGG', 'SHV'), from='1995-01-01', period='years')
+twr <- out$twr
+twrdf <- as.data.frame(twr)
+twrdf$date <- as.Date( rownames(twrdf) )
+plotdfall(twrdf, 'date', size=1)
