9308b81bb6ad6e5d2e9233530afd6747b7408ade
dlhjel <dlhjel@gmail.com>
2021-10-30 14:22:18 -0400

    creates plot of alpha vs beta for portfolio and assets in portfolio

--- portfolio_statistics.r
+++ portfolio_statistics.r
@@ -39,8 +39,9 @@ out <- equityinfo(asset, extract=c('Previous Close', 'P/E Ratio'))
 price <- out$'P. Close'
 
 ## calculate value and weight of each asset
-value  <- shares * price
-weight <- value / sum(value)
+value      <- shares * price
+totalvalue <- sum(value)
+weight <- value / totalvalue
 
 ## get twr for each asset
 out <- equityget(asset, period='months')
@@ -48,23 +49,46 @@ twr <- xts::last(out$twr, n=12*5)
 
 ## get twr for the benchmark
 out <- equityget('SPY', period='months')
-SPY <- as.numeric( xts::last(out$twrbench$SPY,  n=12*5) )
+SPY <- as.numeric( xts::last(out$twr$SPY,  n=12*5) )
 
-## calculate beta for each asset
+## calculate alpha and beta for each asset
 beta  <- NA
 alpha <- NA
 for (i in 1:length(asset)) {
     twr_asset <- as.numeric(twr[,i])
-    out <- alpha_beta(twr_asset, SPY)$beta
+    out <- alpha_beta(twr_asset, SPY)
     beta[i]  <- out$beta
     alpha[i] <- out$alpha
 }
-beta  <- as.data.frame( unlist(t(beta)) )
-alpha <- as.data.frame( unlist(t(alpha)))
-names(beta)  <- asset
-names(alpha) <- asset
+stats <- data.frame(asset, 
+                    shares = as.numeric(shares), 
+                    value=as.numeric(value), 
+                    beta, 
+                    alpha)
 
 ## calculate portfolio beta
-beta_portfolio <- sum( weight * beta )
-beta <- data.frame(beta, portfolio = beta_portfolio)
-print(beta)
+beta_portfolio  <- sum( weight * beta )
+alpha_portfolio <- sum( weight * alpha )
+portfolio <- data.frame(asset  = 'portfolio', 
+               shares = NA, 
+               value  = totalvalue, 
+               beta   = beta_portfolio,
+               alpha  = alpha_portfolio)
+stats <- as_tibble( rbind(stats, portfolio) )
+print(stats)
+
+## plot portfolio
+out <- plotfit(stats$beta, stats$alpha, stats$asset, nofit=TRUE)
+xx <- stats[nrow(stats),]$beta
+yy <- stats[nrow(stats),]$alpha
+color <- as.character(out$legend[nrow(out$legend),]$color)
+points(xx, yy, pch=16, col=color)
+
+## plot interactive
+if (os == 'windows') {
+    ## following uses plotly which does not work on Chromebook
+    plot_interactive(stats, 'beta', 'alpha')
+}
+
+library(shiny)
+shinyplot(as.data.frame(stats), 'beta', 'alpha')
